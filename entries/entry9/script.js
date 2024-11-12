1d485bc4174431b924c95b5eb44597e57e537934
yulansuk <suky173@newschool.edu>
2024-11-12 18:06:45 -0500

    pink like suki

--- entries/entry9/script.js
+++ entries/entry9/script.js
@@ -1,4 +1,3 @@
-// Gallery items with their associated background colors
 const items = [
     { src: 'imgs/blue.png', bgColor: '#266Ca9', textColor: '#ADE1FB', altText: 'Juneberry Redbull' },
     { src: 'imgs/green.png', bgColor: '#2fff05', textColor: '#d3ffb3', altText: 'Elderberry Redbull' },
@@ -7,15 +6,14 @@ const items = [
 
 const galleryImages = document.querySelectorAll('.gallery img');
 
-// Function to update the background color based on the clicked can
+
 function updateBackgroundColor(index) {
-    // Change the background color based on the can clicked
+ 
     document.body.style.backgroundColor = items[index].bgColor;
-    // Optionally update text color
     document.getElementById('gallery-text').style.color = items[index].textColor;
 }
 
-// Add event listeners for each image to trigger the color change
+
 galleryImages.forEach((img, index) => {
     img.addEventListener('click', () => {
         updateBackgroundColor(index); // Update the background color based on the clicked can
